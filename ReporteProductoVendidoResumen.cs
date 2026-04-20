8dc283a02d92b81f52ee442fef0808534572b9cc
angelgabriel10cr-hash <angelgabriel.10.cr@gmail.com>
2026-04-19 19:41:13 -0600

    se hace reporte de produvto vendiodo

---
+++ ReporteProductoVendidoResumen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SVDE.Entidades.Maestros
+{
+    public class ReporteProductoVendidoResumen
+    {
+        public int TotalRegistros { get; set; }
+        public decimal TotalVendido { get; set; }
+    }
+}
